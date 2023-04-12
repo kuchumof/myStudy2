2c69a057959e03ff9a69029769fb846cd3860c5c
Kuchumof <kuchumof@gmail.com>
2023-04-12 18:59:53 +0500

    add change to package zaur.proChak.stream test1

--- src/zaur/proChak/stream/Test1_methodMap.java
+++ src/zaur/proChak/stream/Test1_methodMap.java
@@ -43,5 +43,23 @@ public class Test1_methodMap {
         System.out.println(set2);
         System.out.println(list3);
 
+        /*============================================================================================*/
+
+        Student st1 = new Student("Ivan", 'm', 22, 3, 8.3);
+        Student st2 = new Student("Nikolay", 'm', 28, 2, 6.4);
+        Student st3 = new Student("Elena", 'f', 19, 1, 8.9);
+        Student st4 = new Student("Petr", 'm', 35, 4, 7);
+        Student st5 = new Student("Mariya", 'f', 23, 3, 7.4);
+
+        List<Student> students = new ArrayList<>(); //Здесь должен быть List
+        students.add(st1);
+        students.add(st2);
+        students.add(st3);
+        students.add(st4);
+        students.add(st5);
+
+        System.out.println(students.stream().map(e->e.getName()).collect(Collectors.toList()));
+
+
     }
 }
